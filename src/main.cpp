410ce9a9a8c77d89fd2f9d1136e53a7ef4918d52
kirilldolbin <kirill.dolbin.02@gmail.com>
2025-12-13 03:31:50 -0500

    13.12

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,35 @@
 
 using namespace std::chrono_literals;
 
-// std::atomic<bool> running{true};
+std::atomic<bool> running{true};
 
-// void signalHandler(int signal) {
-//     std::cout << "\nReceived signal " << signal << ", shutting down..." << std::endl;
-//     running = false;
-// }
+void signalHandler(int signal) {
+    std::cout << "\nReceived signal " << signal << ", shutting down..." << std::endl;
+    running = false;
+}
 
-// void customGooseHandler(const std::vector<bool>& values, uint64_t timestamp,
-//                         uint32_t stNum, uint32_t sqNum) {
-//     std::cout << "\n[Custom Handler] GOOSE Event:" << std::endl;
-//     std::cout << "  Sequence: stNum=" << stNum << ", sqNum=" << sqNum << std::endl;
-//     std::cout << "  Timestamp: " << timestamp << std::endl;
-//     std::cout << "  Data: ";
-//     for (size_t i = 0; i < values.size(); ++i) {
-//         std::cout << "[" << i << "]=" << (values[i] ? "1" : "0") << " ";
-//     }
-//     std::cout << std::endl;
-// }
+void customGooseHandler(const std::vector<bool>& values, uint64_t timestamp,
+                        uint32_t stNum, uint32_t sqNum) {
+    std::cout << "\n[Custom Handler] GOOSE Event:" << std::endl;
+    std::cout << "  Sequence: stNum=" << stNum << ", sqNum=" << sqNum << std::endl;
+    std::cout << "  Timestamp: " << timestamp << std::endl;
+    std::cout << "  Data: ";
+    for (size_t i = 0; i < values.size(); ++i) {
+        std::cout << "[" << i << "]=" << (values[i] ? "1" : "0") << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc, char **argv)
 {
 
     GPIO gpio;
+    TOR::GooseSubscriber Minevich("simpleIOGenericIO/LLN0$GO$gcbAnalogValues");
     // bool value = false;
     int i = 0;
     while (true)
     {
-        // for (int i = 0; i < 4; i++)
-        // {
-        //     bool value = true;
-        //     gpio.SetValue(value, i);
-        //     std::this_thread::sleep_for(100ms);
-        //     value = !value;
-        //     gpio.SetValue(value, i);
-        // }
+        
         gpio.SetValue(false, i);
         i++;
         if (i > 3)
@@ -54,85 +48,6 @@ int main(int argc, char **argv)
         gpio.SetValue(true, i);
         std::this_thread::sleep_for(100ms);
     }
+    
 
-    // // Устанавливаем обработчики сигналов
-    // std::signal(SIGINT, signalHandler);
-    // std::signal(SIGTERM, signalHandler);
-
-    // std::string interface = "eth0";
-    // if (argc > 1) {
-    //     interface = argv[1];
-    // }
-
-    // std::cout << "Starting GOOSE Receiver on interface: " << interface << std::endl;
-
-    // try {
-    //     // Создаём GooseReceiver
-    //     auto receiver = GooseReceiver::Create();
-    //     if (interface != "") {
-    //         receiver = std::make_unique<GooseReceiver>(interface);
-    //     }
-
-    //     if (!receiver) {
-    //         std::cerr << "Failed to create GOOSE receiver" << std::endl;
-    //         return 1;
-    //     }
-
-    //     // Создаём GooseSubscriber с тем же GOOSE Control Block Reference, что и у издателя
-    //     GooseSubscriber subscriber("SPbPUIO/oolegkaa");
-
-    //     // Настраиваем подписчика в соответствии с издателем
-    //     uint8_t dstMac[6] = {0x01, 0x0c, 0xcd, 0x01, 0x00, 0x01};
-    //     subscriber.SetDstMac(dstMac);
-    //     subscriber.SetAppId(1000);
-
-    //     // Устанавливаем пользовательский обработчик
-    //     subscriber.SetListener(customGooseHandler);
-
-    //     // Добавляем подписчика в приёмник
-    //     receiver->AddSubscriber(subscriber);
-
-    //     // Запускаем приёмник
-    //     receiver->Start();
-
-    //     if (receiver->IsRunning()) {
-    //         std::cout << "GOOSE Receiver is running. Press Ctrl+C to stop." << std::endl;
-    //         std::cout << "Waiting for GOOSE messages with AppId=1000..." << std::endl;
-
-    //         // Основной цикл
-    //         while (running) {
-    //             // Периодически выводим текущее состояние
-    //             auto values = subscriber.GetDataSetValues();
-
-    //             std::cout << "\rCurrent state - stNum: " << subscriber.GetStNum()
-    //                       << ", sqNum: " << subscriber.GetSqNum()
-    //                       << ", Valid: " << (subscriber.IsValid() ? "Yes" : "No")
-    //                       << ", Values: ";
-    //             for (bool val : values) {
-    //                 std::cout << (val ? "1" : "0") << " ";
-    //             }
-    //             std::cout << "       " << std::flush;
-
-    //             std::this_thread::sleep_for(100ms);
-    //         }
-
-    //         // Останавливаем приёмник
-    //         receiver->Stop();
-    //         std::cout << "\nGOOSE Receiver stopped successfully." << std::endl;
-
-    //     } else {
-    //         std::cerr << "Failed to start GOOSE receiver." << std::endl;
-    //         std::cerr << "Possible reasons:" << std::endl;
-    //         std::cerr << "  - Ethernet interface '" << interface << "' doesn't exist" << std::endl;
-    //         std::cerr << "  - Root permissions are required" << std::endl;
-    //         std::cerr << "  - GOOSE traffic is not arriving on the interface" << std::endl;
-    //         return 1;
-    //     }
-
-    // } catch (const std::exception& e) {
-    //     std::cerr << "Error: " << e.what() << std::endl;
-    //     return 1;
-    // }
-
-    return 0;
 }
---
+++ src/subscr.cpp
@@ -0,0 +1,60 @@
+/*
+ * goose_subscriber_example.c
+ *
+ * This is an example for a standalone GOOSE subscriber
+ *
+ * Has to be started as root in Linux.
+ */
+
+#include "goose_receiver.h"
+#include "goose_subscriber.h"
+#include "hal_thread.h"
+#include "linked_list.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include "subscr.h"
+
+static void
+gooseListener(GooseSubscriber subscriber, void* parameter)
+{
+    printf("GOOSE event:\n");
+    printf("  stNum: %u sqNum: %u\n", GooseSubscriber_getStNum(subscriber),
+            GooseSubscriber_getSqNum(subscriber));
+    printf("  timeToLive: %u\n", GooseSubscriber_getTimeAllowedToLive(subscriber));
+
+    uint64_t timestamp = GooseSubscriber_getTimestamp(subscriber);
+
+    printf("  timestamp: %u.%u\n", (uint32_t) (timestamp / 1000), (uint32_t) (timestamp % 1000));
+    printf("  message is %s\n", GooseSubscriber_isValid(subscriber) ? "valid" : "INVALID");
+
+    MmsValue* values = GooseSubscriber_getDataSetValues(subscriber);
+
+    char buffer[1024];
+
+    MmsValue_printToBuffer(values, buffer, 1024);
+
+    printf("  allData: %s\n", buffer);
+}
+namespace TOR{
+GooseSubscriber::GooseSubscriber(const std::string &goCbRef)
+{
+    receiver = GooseReceiver_create();
+    GooseReceiver_setInterfaceId(receiver, "eth0");
+    ::GooseSubscriber subscriber = GooseSubscriber_create("simpleIOGenericIO/LLN0$GO$gcbAnalogValues", nullptr);
+
+    uint8_t dstMac[6] = {0x01,0x0c,0xcd,0x01,0x00,0x01};
+    GooseSubscriber_setDstMac(subscriber, dstMac);
+    GooseSubscriber_setAppId(subscriber, 1000);
+
+    GooseSubscriber_setListener(subscriber, gooseListener, NULL);
+
+    GooseReceiver_addSubscriber(receiver, subscriber);
+
+    GooseReceiver_start(receiver);
+}
+
+GooseSubscriber::~GooseSubscriber()
+{
+}
+}
